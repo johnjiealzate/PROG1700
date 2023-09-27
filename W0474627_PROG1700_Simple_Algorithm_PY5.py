33de241c434762fec1972e20720d14c654ce8a4d
johnjiealzate <146230970+johnjiealzate@users.noreply.github.com>
2023-09-27 20:56:29 -0300

    Add files via upload

---
+++ W0474627_PROG1700_Simple_Algorithm_PY5.py
@@ -0,0 +1,24 @@
+#Author: Johnjie Alzate
+#Student ID: W0474627
+#Course: PROG1700
+#Project: Simple_Algorithm
+#Repository: https://github.com/johnjiealzate/NSCC_ITGE_Courses/blob/main/W0474627_PROG1700_Simple_Algorithm_PY5.py
+#Programming Language: Python
+#Licence: Creative Commons
+#Date:09-27-23
+#Version: 1.0
+
+#Represents the age as "x" variable
+x = input("Please enter your age: ")
+
+#Exports the YTD-Year to Date as "y" variable
+import datetime
+y = datetime.datetime.now().year
+
+#Displays the year you were born with a notification and provided an error for wrong input type
+if x.isdigit():
+    #Calculates the YOB-Year of Birth
+    z = y - int(x)
+    print("You're awesome! You are born on", z, ".")
+else:
+    print("Please enter a valid value.")
