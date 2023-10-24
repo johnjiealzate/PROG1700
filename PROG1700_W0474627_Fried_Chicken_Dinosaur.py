f4044240b7e404711b037de56fa530310f25598b
johnjiealzate <theflickpoet@gmail.com>
2023-10-24 08:32:08 -0300

    New file

--- PROG1700_W0474627_Fried_Chicken_Dinosaur.py
+++ PROG1700_W0474627_Fried_Chicken_Dinosaur.py
@@ -1,21 +1,17 @@
 # Declare global variables
 chicken_in_box = 20
-days_counter = 0
+days_counter = 1
 chicken_eaten_daily = 0
-chiecken_eaten_when_ill = 0
+chicken_eaten_when_ill = 0
 
-# Program control for fried chickhen dinosaur
-while chicken_in_box > 0:
+# Program control for fried chicken dinosaur
+while days_counter < 60:
     # Start of the program
-    # Input the numbeof days the dinosaur has been eating fried chickhen
-    days_input = input ("Please enter the count of days the dinosaur is eating the fried chicken:")
-
-    if days_input == 1:
-        # Cast the input
-        days_input = int (days_counter)
-        print ("Fried chickhen eaten on day 1 is 1.00." )
-        print ("Chickhen remaining in the box is", chicken_in_box-1)
-    elif days_input == 7:
-        print ()
-else:
-    print ("Number of days it took to run out of chikhen is")    
+    # Cast the input
+    days_counter = int (days_counter)
+    print ("Fried chicken eaten on day", days_counter, "is", days_counter*1, "lb.")
+    if days_counter < 1:
+        
+        break
+        
+        
---
+++ Pong Game.py
@@ -0,0 +1,49 @@
+import turtle
+
+# Define Variables
+score_a = 0
+score_b = 0
+ 
+# Setup the drawing pallet
+wn = turtle.Screen()
+wn.title("Pong")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+ 
+# Define Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.speed(10)
+paddle_a.shapesize(stretch_wid=5,stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350,0)
+ 
+# Define Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.speed(0)
+paddle_b.shapesize(stretch_wid=5,stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350,0)
+ 
+# Define the ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0,0)
+ 
+# Define the ball
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", 
+          font=("Courier", 24, "normal"))
