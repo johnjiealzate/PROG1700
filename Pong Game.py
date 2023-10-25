5c63a4ee2d93414ab3673020da3eba124589ad70
johnjiealzate <theflickpoet@gmail.com>
2023-10-25 15:43:20 -0300

    Update

--- Pong Game.py
+++ Pong Game.py
@@ -36,17 +36,48 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0,0)
+
+# CReate the scoreboard object
+score_board = turtle.Turtle()
+score_board.speed(0)
+score_board.shape("square")
+score_board.color("white")
+score_board.penup()
+score_board.hideturtle()
+score_board.goto(0,260)
+score_board.write("Player A: {} Player B: {}".format(score_a, score_b), align="center",
+                  font=("Courier", 24, "normal"))
  
-# Define the ball
-pen = turtle.Turtle()
-pen.speed(0)
-pen.shape("square")
-pen.color("white")
-pen.penup()
-pen.hideturtle()
-pen.goto(0,260)
-pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", 
-          font=("Courier", 24, "normal"))
+# Define a function to move the paddle up 
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y = y + 20
+    paddle_a.sety(y)
+
+# Define a function to move the paddle down 
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y = y - 20
+    paddle_a.sety(y)
+
+# Define a function to move the paddle up 
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y = y + 20
+    paddle_b.sety(y)
+
+# Define a function to move the paddle down 
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y = y - 20
+    paddle_b.sety(y)
+
+# Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
 
 # Main
 while True:
