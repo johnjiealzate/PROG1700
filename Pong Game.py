71ee3ab8022cc661b2b26978486ab396f9a30abe
johnjiealzate <theflickpoet@gmail.com>
2023-10-26 14:49:13 -0300

    Update code

--- Pong Game.py
+++ Pong Game.py
@@ -14,16 +14,17 @@ wn.tracer(0)
 # Define Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.shape("square")
-paddle_a.color("white")
+paddle_a.color("red")
 paddle_a.speed(10)
 paddle_a.shapesize(stretch_wid=5,stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350,0)
  
-# Define Paddle B
+# Define Paddle B to use the methods
 paddle_b = turtle.Turtle()
+# Method
 paddle_b.shape("square")
-paddle_b.color("white")
+paddle_b.color("blue")
 paddle_b.speed(0)
 paddle_b.shapesize(stretch_wid=5,stretch_len=1)
 paddle_b.penup()
@@ -31,14 +32,19 @@ paddle_b.goto(350,0)
  
 # Define the ball
 ball = turtle.Turtle()
-ball.speed(0)
-ball.shape("square")
-ball.color("white")
+ball.speed(1)
+ball.shape("circle")
+ball.color("yellow")
 ball.penup()
 ball.goto(0,0)
 
-# CReate the scoreboard object
+# Define the ball movement
+ball.dx = 0.05
+ball.dy = 0.05
+
+# CReate the scoreboard object class to use the methods
 score_board = turtle.Turtle()
+# Methods
 score_board.speed(0)
 score_board.shape("square")
 score_board.color("white")
@@ -80,5 +86,16 @@ wn.onkeypress(paddle_b_up, "Up")
 wn.onkeypress(paddle_b_down, "Down")
 
 # Main
-while True:
+playing = True
+while playing:
     wn.update()
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Check Top and Bottom Borders
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy = ball.dy * -1
+    elif ball.ycor() <-290:
+        ball.sety(-290)
+        ball.dy = ball.dy * -1
