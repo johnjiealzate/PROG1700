92e9df76dd9ef81f08b810bdcd5b88c645fc4c93
johnjiealzate <theflickpoet@gmail.com>
2023-11-09 15:31:33 -0400

    Update code

--- PROG 1700_W0474627_Creating and Using a Dictionary.py
+++ PROG 1700_W0474627_Creating and Using a Dictionary.py
@@ -23,11 +23,23 @@ print('\n')
 # Check if the student exists in the dictionary
 # Student name is in the list, print name ans score
 while True:
-    if student_name in student_score:
-        print ("Found")
+    if student_name not in student_score:
+        print ("Your name is not in the file system!")
+        score = input ("What's your score? ")
+        student_score [student_name] = score
+        for key, value in student_score.items():
+            print(f'{key}: {value}')
+        print('\n')
+        break
     # Otherwise  outputs a message "Student name Not Found!"
     else:
-        print ("Student name Not Found!")
+        print ("Your name is found in the file system!")
+        for key, value in student_score.items():
+            print(f'{key}: {value}')
+        print('\n')
+        print ("You've been successfully added in the file system!")
+        break
+        
     
 
 
