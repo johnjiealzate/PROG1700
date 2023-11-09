1122e9cf0b5bf6fce45fdceca0b9d51f2c54d3f3
johnjiealzate <theflickpoet@gmail.com>
2023-11-09 14:59:31 -0400

    New assignment

---
+++ PROG 1700_W0474627_Creating and Using a Dictionary.py
@@ -0,0 +1,32 @@
+# Prompt the user to input a student's name
+student_name = input ("Enter the student's first name: ")
+
+
+# Create a dictionary called `student_scores
+student_score = {
+    'Alice': '90',
+    'Bob': '85',
+    'Charlie' : '78',
+    'David' : '92',
+    'Eve': "88"
+}
+
+# Output the 'student_score' dictionary to display the student names and their scores.
+for key, value in student_score.items():
+    print(f'{key}: {value}')
+print('\n')
+
+# Calculate and print the average test score of all the students in the 'student_scores' dictionary.
+
+
+
+# Check if the student exists in the dictionary
+if student_name in student_score:
+    print (value)
+else:
+    print ("Not Found")
+
+
+# If they do, print their name and score
+
+# otherwise, print a message indicating that the student is not found.
---
+++ PROG 1700_W0474627_Distionaries_other_method.py
@@ -0,0 +1,5 @@
+keys = ['name', 'address', 'IDno.' ]
+values = ['Johnjie Alzate', 'Philippines', 'w0474627']
+mydict = dict()
+
+print (mydict)
