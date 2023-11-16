589c9daf935efae1e024d376e92f7f63c4a0d914
johnjiealzate <theflickpoet@gmail.com>
2023-11-16 15:05:32 -0400

    Update

---
+++ PROG 1700_W0474627_Python_Quiz_Game.py
@@ -0,0 +1,27 @@
+# Import random function
+import random
+
+# Step 1: Create two separatae lists
+programming_languages = ['Python', 'JvaScript', 'C++', 'Ruby', 'Java']
+difficulty_levels = [1, 2, 3, 4, 5]
+
+# Step 2: Combine the two list into a list of tuples
+quiz_data = list(zip(programming_languages, difficulty_levels))
+
+# Step 3: Shuffle the list for a random quiz order
+random.shuffle (quiz_data)
+
+score = 0
+for language, difficulty_levels in quiz_data:
+    quess = int(input (f"What is the difficuly level of {language}? Enter a number from 1 to 5. : "))
+    # Check if the guess is correct
+    if quess == difficulty_levels:
+        print ("You're correct!")
+        score += 1
+    else:
+        print (f"You're incorrrect! the difficulty level of {language} is {difficulty_levels}.")
+print (f"Quiz Complete! Your final score is {score}.")
+
+
+
+
