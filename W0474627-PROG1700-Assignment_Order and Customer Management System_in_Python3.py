1df1399eedac2533e7a967fa6be410cccc2e3e6d
johnjiealzate <theflickpoet@gmail.com>
2023-11-22 15:39:23 -0400

    Code update

--- W0474627-PROG1700-Assignment_Order and Customer Management System_in_Python3.py
+++ W0474627-PROG1700-Assignment_Order and Customer Management System_in_Python3.py
@@ -1,3 +1,32 @@
+"""
+Auhor: Johnjie Alzate
+Student ID: W0474627
+Course: IT Generalist
+Programming Language: Python
+Date: 11-22-23
+Version: 3
+"""
+
+customers = {
+    1: {"name" : "Alice", "contact" : "123-456-7890","orders" : []},
+    2: {"name" : "Bob", "contact" : "987-654-3210","orders" : []}
+}
+for key, value in customers.items():
+        print(f'{key}: {value}')
 
 # Output
-print ("Order and Customer Management Menu:")
+print ("Order and Customer Management Menu:\n",
+       "1. Add Customer\n",
+       "2. Place Order\n",
+       "3. Generate Customer Report\n",
+       "4. Generatae aAll Customer Report\n",
+       "5. Exit\n", 
+       "\n",
+       )
+menu_choice = int(input("Enter your choice (1-5): "))
+
+# Add new customer
+def new_customer (new):
+    print(f"{new} is the new customer.")
+new_customer("emil")
+
