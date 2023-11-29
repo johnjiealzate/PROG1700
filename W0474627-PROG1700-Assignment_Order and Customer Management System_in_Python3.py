161ab9428cbd498885accd7622a136f790eda09e
johnjiealzate <146230970+johnjiealzate@users.noreply.github.com>
2023-11-28 23:36:37 -0400

    Code update

--- W0474627-PROG1700-Assignment_Order and Customer Management System_in_Python3.py
+++ W0474627-PROG1700-Assignment_Order and Customer Management System_in_Python3.py
@@ -19,7 +19,7 @@ while True:
         "1. Add Customer\n",
         "2. Place Order\n",
         "3. Generate Customer Report\n",
-        "4. Generate aAll Customer Report\n",
+        "4. Generate All Customer Report\n",
         "5. Exit\n", 
         "\n",
         )
@@ -54,7 +54,37 @@ while True:
             print("Order placed successfully!")
             print(customers)
         place_order(customer_id, product_name, quantity, unit_price)
-        
-            
-    # Add new customer menu #1       
-    '''elif menu_choice == 2:'''
+    
+    # Generate customer report Menu#3
+    elif menu_choice == 3:
+        customer_id = int(input("Enter customer ID: "))
+        def generate_customer_report(customer_id):
+            global customers
+            customer_name = customer_name.get(customer_id)
+            if customer_name:
+                print(f"\nCustomer Report for {customer_name["name"]}:")
+                print(f"Contact: {customer_name["contact"]}")
+                total_spending = sum(order["total_cost"])
+                print(f"Total spending: ${total_spending: 2f}")
+                print("Orders:")
+                for order in customer_name['orders']:
+                    print(f" Order ID: {order["order_id"]} | Product: {order["product_name"]} | Quantity: {order["quantity"]}")
+            else:
+                print("Customer not found.")
+        generate_customer_report(customer_id)
+                
+    # Generate all customer report Menu4
+    elif menu_choice == 4:
+        def generate_all_customer_reports():
+            global customers
+            for customer_id, _ in customers.items():
+                generate_all_customer_reports(customer_id)
+        generate_all_customer_reports(customer_id)
+    
+    # Exit Menu5
+    elif menu_choice == 5:
+        print("Thank you for using the Order and Customer Management System (OCMS).")
+        break
+    
+    else:
+        print("Invalid choice. Please enter a valid number between 1 and 5.")
