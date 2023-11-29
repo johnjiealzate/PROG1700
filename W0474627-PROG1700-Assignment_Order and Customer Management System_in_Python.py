f779add0ab1d6425e25b8a48180d335fdd76c3b1
johnjiealzate <theflickpoet@gmail.com>
2023-11-29 15:07:57 -0400

    Code update

---
+++ W0474627-PROG1700-Assignment_Order and Customer Management System_in_Python.py
@@ -0,0 +1,94 @@
+"""
+Auhor: Johnjie Alzate
+Student ID: W0474627
+Course: IT Generalist
+Programming Language: Python
+Date: 11-22-23
+Version: 3
+"""
+
+# Create empty dictionary
+customers = {}
+
+# Initial customer/order ID from an empty customers distionary
+customer_id = 0
+order_id = 0
+
+# Menu
+while True:
+    print ("Order and Customer Management Menu:\n",
+        "1. Add Customer\n",
+        "2. Place Order\n",
+        "3. Generate Customer Report\n",
+        "4. Generate All Customer Report\n",
+        "5. Exit\n", 
+        "\n",
+        )
+    # Input for menu selection
+    menu_choice = int(input("Enter your choice (1-5): "))
+
+    # Add customer Menu #1
+    if menu_choice == 1:
+        def add_customer():
+            global customers
+            global customer_id
+            customer_name = input("Enter customer name: ")
+            Customer_contact_number = input("Enter customer contact number: ")
+            customer_id = customer_id + 1
+            customers[customer_id] = {"name": customer_name, "contact": Customer_contact_number, "orders": []}
+            print("Customer added successfully!")
+            print (customers)
+        add_customer()
+        
+    # Place order Menu #2
+    elif menu_choice == 2:
+        customer_id = int(input("Enter customer ID: "))
+        product_name = input("Enter product name: ")
+        quantity = int(input("Enter quantity: "))
+        unit_price = float(input("Enter the unit price:"))
+        def place_order(customer_id, product_name, quantity, unit_price):
+            global customers
+            global order_id
+            order_id = order_id + 1
+            total_cost = quantity * unit_price
+            order = {"order_id": order_id, "product_name": product_name, "quantity": quantity, "total_cost": total_cost}
+            customers[customer_id]["orders"].append(order)
+            print("Order placed successfully!")
+            print(customers)
+        place_order(customer_id, product_name, quantity, unit_price)
+    
+    # Generate customer report Menu#3
+    elif menu_choice == 3:
+        customer_id = int(input("Enter customer ID: "))
+        def generate_customer_report(customer_id):
+            global customers
+            customer_name = customers.get(customer_id)
+            if customer_name:
+                print(f"\nCustomer Report for {customer_name['name']}:")
+                print(f"Contact: {customer_name['contact']}")
+                for order in customer_name["orders"]:
+                    total_spending = order["total_cost"]
+                
+                print(f"Total spending: ${total_spending: 2f}")
+                print("Orders:")
+                for order in customer_name['orders']:
+                    print(f" Order ID: {order['order_id']} | Product: {order['product_name']} | Quantity: {order['quantity']}")
+            else:
+                print("Customer not found.")
+        generate_customer_report(customer_id)
+                
+    # Generate all customer report Menu4
+    elif menu_choice == 4:
+        def generate_all_reports():
+            global customers
+            for customer_id, _ in customers.items():
+                print(generate_customer_report(customer_id))
+        generate_all_reports()
+    
+    # Exit Menu5
+    elif menu_choice == 5:
+        print("Thank you for using the Order and Customer Management System (OCMS).")
+        break
+    
+    else:
+        print("Invalid choice. Please enter a valid number between 1 and 5.")
---
+++ W0474627-PROG1700-Python Exercise Reviewing Python Common Functions.py
@@ -0,0 +1,113 @@
+# String: Concatenation - Create two string variables, str1 and str2, and concatenate them into a third variable result_str.
+str1 = "Johnjie"
+str2 = "Alzate"
+result_str = str1 + str2
+print(result_str)
+
+# String: Substring - Given a string sentence, extract and print the first five characters.
+sentence = "This is a sentence."
+print (sentence[0:4])
+
+# String: Formatting - Create a string that includes variables. Use string formatting to insert values into the string.
+str3 = "count"
+variable = 34
+
+print(f"The count is number {variable}.")
+
+# Loops: Print Number - Using a for loop, print the numbers from 1 to 10.
+for n in range(1,11,1):
+    print(n)
+
+# Other way to print in loop horizontally
+result = ""
+for num in range(1,11,1):
+    result = result + str(num) + " "
+print(result)
+
+# Loops: Factorial - Write a function to calculate the factorial of a number using a while loop.
+import math
+print(math.factorial(9))
+
+def factorial(num1):
+    result = 1
+    while num > 0:
+        result = result * num1
+        num1 =  num1 - 1
+    return result
+print(factorial(9))
+
+# Sets: Intersection - Create two sets, set1 and set2, and find and print their intersection.
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+intersection_set = set1.intersection(set2)
+print(intersection_set)
+
+# List: List Operation - Create a list of numbers. Perform the following operations and print the result
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print (sum(list1) / len(list1))
+print (max(list1))
+print (min(list1))
+
+# List comprehention - Generate a list of squares of numbers from 1 to 10 using list comprehension.
+squares = []
+for x in range (1, 11):
+    squares += [x**2]
+print (squares)
+
+# Tuples unpacking - Create a tuple with three values. Unpack the tuple into three variables and print them.
+tuples1 = ("John", "Bob", "Eve")
+(name1, name2, name3) = tuples1
+print (name1, name2, name3)
+
+# Disctionaries - DIctionary Operations: Create a dictionary representing a person with keys like 'name', 'age', and 'city'. Print each key-value pair.
+person = {
+    "name" : "John",
+    "Age": 25,
+    "City": "Manila"
+}
+for key, value in person.items():
+    print(f"{key}: {value}")
+
+# Nested Dictionary - Create a nested dictionary representing information about students and their grades. Print the average grade for each student.
+student_information = {
+    "Student_1": {
+        "Math": 90,
+        "English": 78
+    },
+    "Student_2": {
+        "Math": 88,
+    },
+    "Student_3": {
+        "Math": 99,
+        "Grade": 97
+    }
+}
+
+for student, grades in student_information.items():
+    average_grade = sum (grades.values()) / len(grades)
+    print(f"{student}'s average grade: {average_grade}.")
+
+# Functions: Functions Basics - Write a function that takes two arguments and returns their sum.
+def my_function(name, surname):
+    print(name + surname)
+my_function("Johnjie", "Alzate")
+
+# Default Values: Modify the previous function to have default values for the arguments.
+def phil_cities (city = "Pasig"):
+    print("One of the Philippine cities is " + city)
+
+phil_cities()
+phil_cities("Manila")
+phil_cities("Taguig")
+
+# File Reasing: Read a text file named 'sample.txt' and print its contents line by line.
+f = open("sample.txt", "r")
+print(f.read())
+
+# File Writing: Write a list of strings to a new text file named 'output.txt'.
+f = open("output.txt", "w")
+f.write("This is a sample written string.")
+f.close()
+
+
+
